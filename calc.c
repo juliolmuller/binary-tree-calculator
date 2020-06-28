090475fffdf2f5d7cb444f5a9ad6e0792c8f3899
Julio Muller <juliodolago@gmail.com>
2020-06-28 11:04:31 -0300

    Created function to convert infix espression to postfix

--- calc.c
+++ calc.c
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STR 100
+#define true 1
+#define false 0
+typedef int boolean;
 
+boolean is_numeric(char);
+int infix_to_postfix(char *);
+
+#define MAX_STR 100
 typedef struct _listNode {
   char string[MAX_STR];
   struct _listNode *next;
@@ -32,11 +38,107 @@ int main()
     if (strcmp(exp, "0") == 0) {
       break;
     }
+    infix_to_postfix(exp);
     list_push(&expressions, exp);
   }
 
   list_print(expressions);
   return 0;
+}
+
+boolean is_numeric(char ch)
+{
+  return (ch >= '0' && ch <= '9');
+}
+
+typedef struct _stack {
+  int *array;
+  int top;
+} STACK;
+
+STACK *stack_create(int length)
+{
+  STACK *stack = (STACK *) malloc(sizeof(STACK));
+  stack->array = (int *) malloc(length * sizeof(int));
+  stack->top = -1;
+
+  return stack;
+}
+
+int stack_is_empty(STACK *stack)
+{
+  return stack->top == -1;
+}
+
+char stack_get(STACK *stack)
+{
+  return stack->array[stack->top];
+}
+
+void stack_push(STACK *stack, char op)
+{
+  stack->array[++stack->top] = op;
+}
+
+char stack_pop(STACK *stack)
+{
+  if (stack_is_empty(stack)) {
+    return '$';
+  }
+  return stack->array[stack->top--];
+}
+
+int stack_precedence(char ch)
+{
+  switch (ch) {
+    case '+':
+    case '-':
+      return 1;
+
+    case '*':
+    case '/':
+      return 2;
+
+    case '^':
+      return 3;
+
+    default:
+      return -1;
+  }
+}
+
+int infix_to_postfix(char *infix)
+{
+  STACK *stack = stack_create(strlen(infix));
+  int i, j;
+  for (i = 0, j = 0; infix[i]; i++) {
+    if (is_numeric(infix[i])) {
+      do {
+        infix[j++] = infix[i++];
+      } while (is_numeric(infix[i]));
+      i--;
+    } else if (infix[i] == '(') {
+      stack_push(stack, infix[i]);
+    } else if (infix[i] == ')') {
+      while (!stack_is_empty(stack) && stack_get(stack) != '(') {
+        infix[j++] = stack_pop(stack);
+      }
+      if (!stack_is_empty(stack) && stack_get(stack) != '(')  {
+        return -1;
+      } else {
+        stack_pop(stack);
+      }
+    } else {
+      while (!stack_is_empty(stack) && stack_precedence(infix[i]) <= stack_precedence(stack_get(stack)))  {
+        infix[j++] = stack_pop(stack);
+      }
+      stack_push(stack, infix[i]);
+    }
+  }
+  while (!stack_is_empty(stack)) {
+    infix[j++] = stack_pop(stack);
+  }
+  infix[j] = '\0';
 }
 
 LIST_NODE *list_create_node(char word[])
